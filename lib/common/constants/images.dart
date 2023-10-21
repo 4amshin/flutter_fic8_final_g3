300a83fda80e4d586478eca9783b9c0b12bc51ee
Rely Arfadillah <shinmendenz@gmail.com>
2023-10-21 15:12:39 +0800

    Absensi Mahasiswa Page UI

--- lib/common/constants/images.dart
+++ lib/common/constants/images.dart
@@ -4,6 +4,7 @@ class Images {
   static const String absensi = 'assets/images/absensi-img.svg';
   static const String topShape = 'assets/images/top-shape.svg';
   static const String curvedLine = 'assets/images/curved-line.svg';
+  static const String map = 'assets/images/maps.png';
 
   static const String khs = 'assets/images/khs.jpg';
   static const String jk = 'assets/images/jk.jpg';
--- lib/pages/mahasiswa/pages/absensi_page.dart
+++ lib/pages/mahasiswa/pages/absensi_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fic8_final_g3/common/constants/colors.dart';
+import 'package:flutter_fic8_final_g3/common/constants/images.dart';
+import 'package:flutter_fic8_final_g3/common/widgets/svg_image.dart';
+import 'package:flutter_fic8_final_g3/pages/mahasiswa/widgets/absensi_page_widgets/ap_history.dart';
+import 'package:flutter_fic8_final_g3/pages/mahasiswa/widgets/absensi_page_widgets/ap_map.dart';
+import 'package:flutter_fic8_final_g3/pages/mahasiswa/widgets/absensi_page_widgets/ap_scan_button.dart';
 
 class AbsensiPage extends StatelessWidget {
   const AbsensiPage({Key? key}) : super(key: key);
@@ -7,7 +13,36 @@ class AbsensiPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Absensi Page'),
+        backgroundColor: ColorName.primary,
+        centerTitle: true,
+        title: const Text(
+          "Absensi Kuliah",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 15.0,
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+      body: const SafeArea(
+        minimum: EdgeInsets.symmetric(
+          horizontal: 25,
+          vertical: 10,
+        ),
+        child: Column(
+          children: [
+            SvgImage(
+              iconUrl: Images.absensi,
+              height: 200,
+            ),
+            ApScanButton(),
+            SizedBox(height: 20),
+            ApMap(),
+            SizedBox(height: 20),
+            ApHistory(),
+          ],
+        ),
       ),
     );
   }
